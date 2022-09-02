acfc96bcf21e53327e17a2dc33d752d622cdbe3a
Lukxi <Lukxi@users.noreply.github.com>
2022-09-03 01:30:10 +0200

    added Command Test

---
+++ src/main/java/me/lukxi/recoveritems/commands/Test.java
@@ -0,0 +1,28 @@
+package me.lukxi.recoveritems.commands;
+
+
+import me.lukxi.recoveritems.npc.DeadPlayerManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.*;
+
+public class Test implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+        Player p = (Player) sender;
+        if (DeadPlayerManager.getDeadPlayer().containsKey(p)){
+            new DeadPlayerManager().removeNPC(p);
+            new DeadPlayerManager().createNPC(p, p.getName());
+        }
+        new DeadPlayerManager().createNPC(p, p.getName());
+
+        return false;
+    }
+
+
+}
