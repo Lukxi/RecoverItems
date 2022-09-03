6d84bd12016a9c42bb4666a860bbbbf22b0c1229
Lukxi <Lukxi@users.noreply.github.com>
2022-09-03 12:19:25 +0200

    Added inventory and armor contents

--- src/main/java/me/lukxi/recoveritems/npc/DeadPlayer.java
+++ src/main/java/me/lukxi/recoveritems/npc/DeadPlayer.java
@@ -3,9 +3,9 @@ package me.lukxi.recoveritems.npc;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.server.level.EntityPlayer;
 import net.minecraft.server.level.WorldServer;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
 
-import java.util.UUID;
 
 public class DeadPlayer {
 
@@ -13,13 +13,22 @@ public class DeadPlayer {
     private final GameProfile gameProfile;
     private final WorldServer world;
     private final String name;
+    private final ItemStack[] inv;
+    private final ItemStack[] armor;
+    private final ItemStack mainHand;
+    private final ItemStack offHand;
 
 
-    public DeadPlayer(EntityPlayer npc, GameProfile gameProfile, WorldServer world, String name, Player p){
+    public DeadPlayer(EntityPlayer npc, GameProfile gameProfile, WorldServer world, String name, ItemStack[] inv, ItemStack[] armor,
+                      ItemStack mainHand, ItemStack offHand){
         this.npc = npc;
         this.gameProfile = gameProfile;
         this.world = world;
         this.name = name;
+        this.inv = inv;
+        this.armor = armor;
+        this.mainHand = mainHand;
+        this.offHand = offHand;
     }
 
     public String getName(){
@@ -34,5 +43,16 @@ public class DeadPlayer {
     public EntityPlayer getEntityplayer(){
         return npc;
     }
-
+    public ItemStack[] getInv() {
+        return inv;
+    }
+    public ItemStack[] getArmor() {
+        return armor;
+    }
+    public ItemStack getMainHand() {
+        return mainHand;
+    }
+    public ItemStack getOffHand() {
+        return offHand;
+    }
 }
