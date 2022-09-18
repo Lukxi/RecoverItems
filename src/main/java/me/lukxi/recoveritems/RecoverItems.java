7f2ad259c05414acf397c9bea8571ebc49bc7ade
Lukxi <Lukxi@users.noreply.github.com>
2022-09-18 22:50:43 +0200

    implemented ProtocolLib

--- src/main/java/me/lukxi/recoveritems/RecoverItems.java
+++ src/main/java/me/lukxi/recoveritems/RecoverItems.java
@@ -1,9 +1,14 @@
 package me.lukxi.recoveritems;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import me.lukxi.recoveritems.commands.Test;
 import me.lukxi.recoveritems.listener.PlayerDeath;
 import me.lukxi.recoveritems.listener.PlayerJoin;
 import me.lukxi.recoveritems.listener.PlayerQuit;
+import me.lukxi.recoveritems.utils.ProtocolLibReader;
+import me.oxolotel.utils.bukkit.menuManager.InventoryMenuManager;
+import me.oxolotel.utils.wrapped.module.ModuleManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,10 +26,14 @@ public class RecoverItems extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        ModuleManager.loadModule(ModuleManager.getPluginModuleByClass(InventoryMenuManager.class));
+
         getCommand("Test").setExecutor(new Test());
         Bukkit.getPluginManager().registerEvents(new PlayerDeath(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerJoin(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerQuit(), this);
+        ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+        new ProtocolLibReader().readNPCClickPacket(protocolManager, this);
     }
 
     @Override
